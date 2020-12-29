39926e0add28c920c57c0fc0513d883778de249d
LinskRuis.32G <mgr9525@qq.com>
2020-12-29 10:50:47 +0800

    o?

--- build.bat
+++ build.bat
@@ -1,7 +1,9 @@
-set PATH=D:\ruisCenter\program\msys64\mingw64\bin;C:\Users\Administrator\.cargo\bin;D:\ruisCenter\program\msys64\mingw64\bin
+set PATH=D:\ruisCenter\program\msys64\mingw32\bin;C:\Users\Administrator\.cargo\bin;
 
-set LIBCLANG_PATH=D:\ruisCenter\program\msys64\mingw64\bin
+set RUST_BACKTRACE=1
+@REM set LIBCLANG_PATH=D:\ruisCenter\program\msys64\mingw64\bin
 
 set FFMPEG_DIR=D:\ruisCenter\docker\ubuntu-center\programs\FFmpeg\bin\mingw32
 
-cargo build
+clang -v
+cargo build --target i686-pc-windows-gnu
